4ea73b4937f44a75749251049c8dd7fd72805406
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-06-26 14:50:13 +0200

    feat: add get_dataset api

--- api/index.py
+++ api/index.py
@@ -1,13 +1,25 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import os
+import pandas as pd
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, world'
+    return 'Hello, World!'
 
 
 @app.route('/test')
 def test():
     return 'Test'
+
+@app.route('/api/get_dataset')
+def get_dataset():
+    train_X = pd.read_csv(os.getcwd()+'\\static\\'+'\\train_x.csv', header=None)
+    train_y = pd.read_csv(os.getcwd()+'\\static\\'+'\\train_y.csv', header=None)
+    
+    result = {
+        "trainingSet": train_X,
+        "labelSet": train_y
+    }
+    return jsonify(result)
